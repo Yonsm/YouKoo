3f72465e15bac9ae19778f0efd26a30f3415e2d0
Yonsm <I@YONSM.NET>
2017-12-06 16:16:15 +0800

    Lookup Video Caches

--- Sources/AppDelegate.h
+++ Sources/AppDelegate.h
@@ -3,51 +3,15 @@
 #import "MobileDeviceAccess.h"
 
 //
-@interface AppDelegate : NSObject <NSApplicationDelegate, MobileDeviceAccessListener, NSTextFieldDelegate>
+@interface AppDelegate : NSObject <NSApplicationDelegate, MobileDeviceAccessListener, NSTableViewDelegate, NSTableViewDataSource>
 {
-	NSInteger _selectedColor;
-	IBOutlet NSButton *infoPane;
-	
-	IBOutlet NSTextField *typeField;
-	IBOutlet NSTextField *reguField;
-	IBOutlet NSTextField *modelField;
-	IBOutlet NSTextField *regionField;
-	
-	IBOutlet NSTextField *colorLabel;
-	IBOutlet NSSegmentedControl *colorField;
-	IBOutlet NSTextField *verField;
-	IBOutlet NSTextField *buildField;
-
-	IBOutlet NSTextField *snField;
-	IBOutlet NSTextField *ecidField;
-	IBOutlet NSTextField *btField;
-	IBOutlet NSTextField *wifiField;
-
-	IBOutlet NSTextField *ethField;
-	IBOutlet NSTextField *mlbField;
-
-	IBOutlet NSTextField *imeiField;
-	IBOutlet NSTextField *bbsnField;
-	IBOutlet NSTextField *bbverField;
-
-	IBOutlet NSTextField *cnameField;
-	IBOutlet NSTextField *cverField;
-
-	IBOutlet NSTextField *postLabel;
-	IBOutlet NSTextField *prlField;
-	IBOutlet NSTextField *priField;
-	IBOutlet NSTextField *cidField;
-
-	IBOutlet NSTextField *keyField;
-	IBOutlet NSTextField *valField;
-
-	IBOutlet NSButton *fetchButton;
-	IBOutlet NSButton *restartButton;
-	IBOutlet NSButton *shutdownButton;
-	IBOutlet NSButton *activateButton;
-
+	IBOutlet NSTableView *_tableView;
 	IBOutlet NSButton *deployButton;
 	IBOutlet NSProgressIndicator *activityIndicator;
+	
+	BOOL _sortAscending;
+	NSMutableArray *_caches;
+	NSMutableDictionary *_metas;
 }
 
 @property (assign) IBOutlet NSWindow *window;
--- Sources/AppDelegate.mm
+++ Sources/AppDelegate.mm
@@ -1,6 +1,7 @@
 
 
 #import "AppDelegate.h"
+#import "DeviceHelper.h"
 
 //
 @implementation AppDelegate
@@ -9,23 +10,17 @@
 #pragma mark -
 
 //
-- (void)deviceConnected:(AMDevice*)device;
+- (void)deviceConnected:(AMDevice*)device
 {
+	[self reloadData:device];
+	
 	deployButton.enabled = YES;
-	fetchButton.enabled = YES;
-	restartButton.enabled = YES;
-	shutdownButton.enabled = YES;
-	activateButton.enabled = YES;
 }
 
 //
-- (void)deviceDisconnected:(AMDevice*)device;
+- (void)deviceDisconnected:(AMDevice*)device
 {
-	fetchButton.enabled = NO;
 	deployButton.enabled = NO;
-	restartButton.enabled = NO;
-	shutdownButton.enabled = NO;
-	activateButton.enabled = NO;
 }
 
 #pragma mark -
@@ -34,11 +29,12 @@
 - (void)applicationWillFinishLaunching:(NSNotification *)aNotification
 {
 	window.title = [NSString stringWithFormat:@"%@ - %@", NSBundleName(), NSBundleVersion()];
-	
-	_selectedColor = -1;
-	
+
 	//
 	MobileDeviceAccess.singleton.listener = self;
+	
+	_tableView.target = self;
+	_tableView.doubleAction = @selector(doubleClick:);
 }
 
 //
@@ -73,6 +69,8 @@
 	{
 		NSString *result = nil;
 		
+		//for ()
+		
 		[self performSelectorOnMainThread:@selector(deployed:) withObject:result waitUntilDone:YES];
 	}
 }
@@ -87,6 +85,181 @@
 }
 
 #pragma mark -
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView
+{
+	return _caches.count;
+}
+
+//
+- (nullable id)tableView:(NSTableView *)tableView objectValueForTableColumn:(nullable NSTableColumn *)tableColumn row:(NSInteger)row
+{
+	NSString *identifier = tableColumn.identifier;
+	return _caches[row][identifier];
+}
+
+//
+- (void)tableView:(NSTableView *)tableView didClickTableColumn:(NSTableColumn *)tableColumn
+{
+	_sortAscending = !_sortAscending;
+	[_caches sortUsingComparator:^NSComparisonResult(NSDictionary *obj1, NSDictionary *obj2) {
+		if (_sortAscending)
+		{
+			return [obj1[tableColumn.identifier] localizedCompare:obj2[tableColumn.identifier]];
+		}
+		else
+		{
+			return [obj2[tableColumn.identifier] localizedCompare:obj1[tableColumn.identifier]];
+		}
+	}];
+	[_tableView reloadData];
+}
+
+#pragma mark -
+
+//
+- (void)reloadData:(AMDevice*)device
+{
+	if (_metas == nil)
+	{
+		NSString *path = NSCacheSubPath(@"YouKooMetas.plist");
+		_metas = [NSMutableDictionary dictionaryWithContentsOfFile:path] ?: [NSMutableDictionary dictionary];
+	}
+
+	AFCApplicationDirectory *dir = [device newAFCApplicationDirectory:@"net.yonsm.Armor"];
+	NSArray *documents = [dir directoryContents:@"Documents"];
+	documents = [documents sortedArrayUsingSelector:@selector(compare:)];
+	_caches = [NSMutableArray arrayWithCapacity:documents.count];
+	for (NSString *document in documents)
+	{
+		NSString *path = [@"Documents" stringByAppendingPathComponent:document];
+		NSDictionary *info = [dir getFileInfo:path];
+		if ([info[@"st_ifmt"] isEqualToString:@"S_IFDIR"])
+		{
+			NSArray *items = [dir directoryContents:path];
+			NSMutableArray *videos = [NSMutableArray arrayWithCapacity:items.count];
+			for (NSString *item in items)
+			{
+				if ([item hasSuffix:@".flv"] || [item hasSuffix:@".mp4"])
+				{
+					[videos addObject:item];
+				}
+			}
+			if (videos.count)
+			{
+				NSMutableDictionary *cache = [NSMutableDictionary dictionary];
+				cache[@"VideoId"] = document;
+				//cache[@"Videos"] = videos;
+				if (_metas[document])
+				{
+					[cache addEntriesFromDictionary:_metas[document]];
+				}
+				cache[@"VideoCount"] = [NSString stringWithFormat:@"%ld", videos.count];
+				[_caches addObject:cache];
+			}
+		}
+	}
+	[_tableView reloadData];
+	
+	[self performSelectorInBackground:@selector(reloadMeta:) withObject:_caches];
+}
+
+//
+- (void)reloadMeta:(NSArray *)caches
+{
+	@autoreleasepool
+	{
+		@synchronized(self)
+		{
+			BOOL dirty = NO;
+			for (NSDictionary *cache in caches)
+			{
+				NSString *videoId = cache[@"VideoId"];
+				if (_metas[videoId])
+				{
+					//[self performSelectorOnMainThread:@selector(loadMeta:) withObject:videoId waitUntilDone:YES];
+					continue;
+				}
+
+				NSString *url = [NSString stringWithFormat:@"https://v.youku.com/v_show/id_%@", videoId];
+				NSURL *URL = [NSURL URLWithString:url];
+				NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:URL];
+				[request setValue:@"http://www.youku.com/" forHTTPHeaderField:@"Referer"];
+				[request setValue:@"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36" forHTTPHeaderField:@"User-Agent"];
+				NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+				if (data)
+				{
+					NSString *html = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+					NSString *subtitleSpan = @"<span id=\"subtitle\" title=\"";
+					NSRange start = [html rangeOfString:subtitleSpan];
+					if (start.location != NSNotFound)
+					{
+						NSInteger location = start.location + subtitleSpan.length;
+						NSRange end = [html rangeOfString:@"\"" options:0 range:NSMakeRange(location, html.length - location)];
+						if (end.location != NSNotFound)
+						{
+							NSString *subtitle = [html substringWithRange:NSMakeRange(location, end.location - location)];
+							NSLog(@"%@",subtitle);
+							
+							NSRange end = [html rangeOfString:@"</span>：" options:NSBackwardsSearch range:NSMakeRange(0, location)];
+							if (end.location != NSNotFound)
+							{
+								NSString *titleSpan = @"<span>";
+								NSRange start = [html rangeOfString:titleSpan options:NSBackwardsSearch range:NSMakeRange(0, end.location)];
+								if (start.location != NSNotFound)
+								{
+									NSInteger location = start.location + titleSpan.length;
+									NSString *title = [html substringWithRange:NSMakeRange(location, end.location - location)];
+									NSLog(@"%@",title);
+									
+									NSDictionary *meta = @{@"Subtitle": subtitle ?: @"",
+														   @"Title": title ?: @""};
+									_metas[videoId] = meta;
+									dirty = YES;
+									[self performSelectorOnMainThread:@selector(loadMeta:) withObject:videoId waitUntilDone:YES];
+								}
+							}
+						}
+					}
+				}
+			}
+
+			if (dirty)
+			{
+				NSString *path = NSCacheSubPath(@"YouKooMetas.plist");
+				[_metas writeToFile:path atomically:YES];
+			}
+		}
+	}
+}
+
+//
+- (void)loadMeta:(NSString *)videoId
+{
+	NSUInteger count = _caches.count;
+	for (NSUInteger i = 0; i < count; i++)
+	{
+		NSMutableDictionary *cache = _caches[i];
+		if ([cache[@"VideoId"] isEqualToString:videoId])
+		{
+			[cache addEntriesFromDictionary:_metas[videoId]];
+			[_tableView reloadDataForRowIndexes:[NSIndexSet indexSetWithIndex:i] columnIndexes:[NSIndexSet indexSetWithIndexesInRange:NSMakeRange(2, 2)]];
+			[_tableView setNeedsDisplay];
+			break;
+		}
+	}
+}
+
+#pragma -
+
+//
+- (void)doubleClick:(id)object
+{
+	NSInteger row = [_tableView clickedRow];
+	NSString *videoId = _caches[row][@"VideoId"];
+	NSString *url = [NSString stringWithFormat:@"https://v.youku.com/v_show/id_%@", videoId];
+	[[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:url]];
+}
 
 @end
 
